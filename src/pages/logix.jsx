3d17c7ba8a136cba21d81212dbcb0d414e509b85
Rishabh Ratan <rishabhkarondiya@gmail.com>
2024-07-22 12:15:12 +0530

    check serverurl

--- src/pages/logix.jsx
+++ src/pages/logix.jsx
@@ -17,6 +17,7 @@ const Login = () => {
   }
   const handleSubmit=async(e)=>{
     e.preventDefault()
+    console.log('s-url',process.env.SERVER_URL)
     const res = await login(data)
     if(res.data){
     const userInfo = await res?.data[0]
